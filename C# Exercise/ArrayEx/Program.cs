192b8f325c3a8e73836ec5676f3a91c5ddab361c
Kaleab Cherinet <kalecherinet@gmail.com>
2021-11-22 18:25:46 +0300

    new ArrayEx project

---
+++ C# Exercise/ArrayEx/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArrayEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
