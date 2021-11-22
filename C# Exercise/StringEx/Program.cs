1f8e1c22b74c863fa827824f637a8f1634c6b3d4
Kaleab Cherinet <kalecherinet@gmail.com>
2021-11-23 00:46:22 +0300

    Exercises on string

---
+++ C# Exercise/StringEx/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace StringEx
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            /*
+            var fullName = "kaleab cherinet ";
+
+            //* trim() -  remove white space
+            Console.WriteLine("trim '{0}'", fullName.Trim());
+
+            //* ToUpper() - to uppercase
+            Console.WriteLine("To upper '{0}'", fullName.ToUpper());
+
+            //* length
+            Console.WriteLine("length: {0}", fullName.Length);
+
+            //* indexOf() - index
+            var index = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, index);
+            var lastName = fullName.Substring(++index);
+            Console.WriteLine("first name: " + firstName);
+            Console.WriteLine("last name: " + lastName);
+
+            //*split() - split them into different arrays
+            var names = fullName.Split(' ');
+            Console.WriteLine("first name: " + names[0]);
+            Console.WriteLine("last name: " + names[1]);
+
+            //* Replace() -  replace anything with given input
+            var repSpaceName = fullName.Replace(' ', '/');
+            Console.WriteLine(repSpaceName);
+            var repCharName = fullName.Replace('a', 'z');
+            Console.WriteLine(repCharName);
+            var repStringName = fullName.Replace("kaleab", "z");
+            Console.WriteLine(repStringName);
+
+            //* IsNullOrWhiteSpace()
+            if (string.IsNullOrEmpty(" ")) ;
+            Console.WriteLine("invalid");
+            */
+        }
+    }
+}
